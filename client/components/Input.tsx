b64a32c44263bc71d7cb61bc240fc218177c40a2
nikobatzi1990 <nicole.boci@gmail.com>
2025-07-11 23:51:18 -0400

    feat: input field label

--- client/components/Input.tsx
+++ client/components/Input.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 interface InputProps {
   autoComplete: 'email' | 'username' | 'password' | 'off';
@@ -10,11 +10,13 @@ const Input = ({ autoComplete }: InputProps) => {
 
   return (
   <View>
+    <Text>Email Address</Text>
     <TextInput
       style={styles.input}
       onChangeText={onChangeText}
       value={text}
       autoComplete={autoComplete ?? 'off'}
+      accessibilityLabel="email address input field"
     />
   </View>
   );
